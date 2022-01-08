0fce71c01c041cadb4b225c33f2f16783da59a98
sagar <sagarshinde500@gmail.com>
2022-01-08 15:49:10 +0530

    check in done

---
+++ src/test/java/Golden/MobileTest.java
@@ -0,0 +1,18 @@
+package Golden;
+import org.testng.annotations.Test;
+public class MobileTest {
+	
+@Test
+public void Android()
+{
+	System.out.println("Runing on Android");
+}
+@Test
+public void iOS()
+{
+	System.out.println("Runing on iOS");
+	
+}
+}
+
+
---
+++ src/test/java/Golden/WebTest.java
@@ -0,0 +1,19 @@
+package Golden;
+
+import org.testng.annotations.Test;
+
+public class WebTest {
+	@Test
+	public void Chrome()
+	{
+		System.out.println("Runing on Chrome");
+	}
+	@Test
+	
+public void Mozila()
+	{
+		System.out.println("Runing on Mozilaa");
+		
+	}
+
+}
